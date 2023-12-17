79ead2ed462c597ab8a5314371a6aff3e2afc85f
Snorre Sæther <snosaet@hotmail.com>
2023-12-17 16:18:12 +0100

    moves some styling from notification to screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.scss";
 import { Screen } from "./Components/Screen/Screen.tsx";
-import { Notification } from "./Components/Notification/Notification.tsx";
 
 function App() {
   return (
     <>
-      <Screen>
-        <Notification />
-      </Screen>
+      <Screen></Screen>
     </>
   );
 }
--- frontend/src/Components/Notification/Notification.tsx
+++ frontend/src/Components/Notification/Notification.tsx
@@ -8,15 +8,13 @@ type NotificationProps = {
 export function Notification({ onClick }: NotificationProps) {
   return (
     <>
-      <div className={styles.screen}>
-        <div className={styles.notificationContainer} onClick={onClick}>
-          <img src={icon} alt="app icon" className={styles.icon} />
-          <div className={styles.notificationText}>
-            <h5 className={styles.notificationHeader}>Snorre</h5>
-            <p className={styles.notificationContent}>
-              Hei! Jeg heter Snorre :) Lær mer om meg her ved å trykke!
-            </p>
-          </div>
+      <div className={styles.notificationContainer} onClick={onClick}>
+        <img src={icon} alt="app icon" className={styles.icon} />
+        <div className={styles.notificationText}>
+          <h5 className={styles.notificationHeader}>Snorre</h5>
+          <p className={styles.notificationContent}>
+            Hei! Jeg heter Snorre :) Lær mer om meg her ved å trykke!
+          </p>
         </div>
       </div>
     </>
