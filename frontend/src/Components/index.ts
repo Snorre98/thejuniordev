e87e8e3704e0b3bc0e48f8d69d7d0a58a87a74f6
Snorre Jr <snosaet@gmail.com>
2024-09-21 15:19:10 +0200

    removes button export

--- frontend/src/Components/index.ts
+++ frontend/src/Components/index.ts
@@ -1,6 +1,5 @@
 export { AppButton } from "./AppButton";
 export { Page } from "./Page";
-export { Button } from "./Button";
 export { Screen } from "./Screen";
 export { Watch } from "./Watch";
 export { Line } from "./Line";
