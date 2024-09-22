1924c3b832d7f691b1514eaad80c799f5a155767
Snorre Jr <snosaet@gmail.com>
2024-09-22 14:02:55 +0200

    remove unused stuff

--- frontend/src/Components/Screen/components/Bezel/Bezle.tsx
+++ frontend/src/Components/Screen/components/Bezel/Bezle.tsx
@@ -1,15 +1,26 @@
 import { Children } from "../../../../types";
 import styles from "./Bezel.module.scss";
+import {useStore} from "../../../../store"
 
 type BezelProps = {
   children?: Children;
 };
 
 export function Bezel({ children }: BezelProps) {
+  const { setScreen } = useStore();
+
+  const handleLock = () => {
+    setScreen('lock');
+  };
   return (
-    <div className={styles.bezel}>
+    <div>
+         <span className={styles.lockButton} onClick={handleLock}/>
+<div className={styles.bezel}>
       <span className={styles.notch} />
+   
       {children}
     </div>
+    </div>
+    
   );
 }
