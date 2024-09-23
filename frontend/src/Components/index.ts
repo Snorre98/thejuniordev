238dfb0c308fd1b61ee139246e3f1178197b1893
Snorre Jr <snosaet@gmail.com>
2024-09-23 05:47:31 +0200

    rmoves dead code

--- frontend/src/Components/index.ts
+++ frontend/src/Components/index.ts
@@ -1,6 +1,5 @@
 export { AppButton } from "./AppButton";
 export { Page } from "./Page";
-export { OldScreen } from "./OldScreen";
 export { Watch } from "./Watch";
 export { Message } from "./Message";
 export { Screen } from "./Screen";
