7e334f8ea2cd84869eac6921f559450c59f5cb21
Snorre Jr <snosaet@gmail.com>
2024-10-05 05:37:17 +0200

    fixed stylelin

--- frontend/src/Display/ProjectDisplay/ProjectDisplay.tsx
+++ frontend/src/Display/ProjectDisplay/ProjectDisplay.tsx
@@ -1,9 +1,35 @@
 import { Screen } from "../../Components/Screen";
+import SoundsGood from "../../assets/sounds-good.jpg";
+import styles from "./ProjectDisplay.module.scss";
+
+const mockProps = {
+	projectTitle: "Sounds Good",
+	projectIcon: SoundsGood,
+};
 
 export function ProjectDisplay() {
 	return (
 		<Screen>
-			<h1>test</h1>
+			<div className={styles.projectContainer}>
+				<div className={styles.projectHeader}>
+					<div className={styles.blurredBackground}>
+						<img
+							src={mockProps.projectIcon}
+							alt="project-icon"
+							className={styles.blurredIcon}
+						/>{" "}
+						<div className={styles.projectIconWrapper}>
+							<img
+								src={mockProps.projectIcon}
+								alt="project-icon"
+								className={styles.projectIcon}
+							/>
+						</div>
+					</div>
+
+					<h2 className={styles.projectTitle}>{mockProps.projectTitle}</h2>
+				</div>
+			</div>
 		</Screen>
 	);
 }
