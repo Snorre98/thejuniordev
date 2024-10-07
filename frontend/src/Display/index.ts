836a4334c03f19e4b2ddad19a4b51fdc36624908
Snorre Jr <snosaet@gmail.com>
2024-10-07 04:02:28 +0200

    adds error and loading screens

--- frontend/src/Display/index.ts
+++ frontend/src/Display/index.ts
@@ -1,6 +1,8 @@
 export { BioDisplay } from "./BioDisplay";
 export { ChatDisplay } from "./ChatDisplay";
+export { ErrorDisplay } from "./ErrorDisplay";
 export { HomeDisplay } from "./HomeDisplay";
+export { LoadingDisplay } from "./LoadingDisplay";
 export { LockDisplay } from "./LockDisplay";
 export { MessagesDisplay } from "./MessagesDisplay";
 export { ProjectDisplay } from "./ProjectDisplay";
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2,4 +2,12 @@ import type { ReactNode } from "react";
 
 export type Children = ReactNode;
 
-export type Screens = "lock" | "home" | "messages" | "bio" | "chat" | "project";
+export type Screens =
+	| "lock"
+	| "home"
+	| "messages"
+	| "bio"
+	| "chat"
+	| "project"
+	| "loading"
+	| "error";
