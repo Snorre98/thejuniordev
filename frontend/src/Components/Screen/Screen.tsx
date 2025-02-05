d4f97c6417c8adce8f096a5b6ed5e513c3f815d7
Snorre Jr <snosaet@gmail.com>
2025-02-05 13:18:37 +0100

    fixes Screen.tsx ts error

--- frontend/src/Components/Screen/Screen.tsx
+++ frontend/src/Components/Screen/Screen.tsx
@@ -6,19 +6,15 @@ import { Bezel, Line } from "./components";
 export interface ScreenProps {
 	children?: ReactNode;
 	onPullUp?: () => void;
-	//onUnlock?: () => void; TODO - #1
 	onBack?: () => void;
 }
 
 export function Screen({
 	children,
 	onPullUp,
-	onBack,
-	//	...props  TODO - #1
 }: ScreenProps) {
 	const { backgrounds, defaultBackground, currentScreen } = useStore();
 	const background = backgrounds[currentScreen] || defaultBackground;
-	const test = onBack; // TODO - #1
 
 	return (
 		<Bezel>
