523754622177700f2de14df9ec77f09f0f3ac5c4
Snorre Jr <snosaet@gmail.com>
2025-02-05 20:40:58 +0100

    adds relative path import

--- frontend/supabaseClient.d.ts
+++ frontend/supabaseClient.d.ts
@@ -1,4 +1,4 @@
-import { SupabaseClient } from '@supabase/supabase-js';
+import type { SupabaseClient } from "@supabase/supabase-js";
 
 declare const supabase: SupabaseClient;
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-    modules: {
-      localsConvention: 'camelCaseOnly',
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"~": path.resolve(__dirname, "./"),
+		},
+	},
+	css: {
+		postcss: "./postcss.config.js",
+		modules: {
+			localsConvention: "camelCaseOnly",
+			generateScopedName: "[name]__[local]___[hash:base64:5]",
+		},
+	},
+});
